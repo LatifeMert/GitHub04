a21dcd639611245ece5eb438648f91f246314803
LatifeMert <latifemert.01@gmail.com>
2023-03-31 02:46:17 +0300

    4.gun

---
+++ src/G02.java
@@ -0,0 +1,25 @@
+public class G02 {
+    /*
+    Git-GitHub'da projelerimizin uzerinde calisirken temelde 3 adimda islem yapiyoruz
+
+    1-Projelerimizi/dosyalarimizi git ile iliskilendiriyorduk.
+              git init
+
+     2-Projemizde/dosyamizda bulunanlari versiyon olusturuyorduk
+
+     git add .
+     git commit -m"versionadi"
+
+
+    3-gitHUb 'a yollama/entegrasyonu
+
+    git remote add origin repoUrlAdresi
+    already origin
+    git remote set-url repoUrlAdresi
+
+    git push
+
+
+
+     */
+}
